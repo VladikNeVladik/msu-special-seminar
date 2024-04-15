4f0373e1e3d0a6bd6ae5288224d2af4b36b675f8
Vladislav Aleinik <valeinik@ispras.ru>
2024-04-16 00:44:58 +0300

    02_async_io: implement synchronous copy operation

---
+++ 02_async_io/common.h
@@ -0,0 +1,91 @@
+// No copyright. 2024, Vladislav Aleinik
+#ifndef MSUSEM_ASYNC_IO
+#define MSUSEM_ASYNC_IO
+
+#define _GNU_SOURCE
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+//=================
+// File operations
+//=================
+
+void open_src_file(const char* filename, int* fd, uint32_t* file_size)
+{
+    *fd = open(filename, O_RDONLY|O_DIRECT);
+    if (*fd == -1)
+    {
+        fprintf(stderr, "Unable to open source file '%s': errno=%i (%s)",
+            filename, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    struct stat statbuf;
+    if (fstat(*fd, &statbuf) == -1)
+    {
+        fprintf(stderr, "Unable to determine source file size: errno=%i (%s)",
+            errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    *file_size = statbuf.st_size;
+}
+
+void open_dst_file(const char* filename, int* fd, uint32_t src_size)
+{
+    *fd = open(filename, O_WRONLY|O_CREAT|O_TRUNC, 0644);
+    if (*fd == -1)
+    {
+        fprintf(stderr, "Unable to open destination file '%s': errno=%i (%s)",
+            filename, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (fallocate(*fd, 0, 0, src_size) == -1)
+    {
+        fprintf(stderr, "Not enough space for file '%s': errno=%i (%s)",
+            filename, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+void close_src_dst_files(
+    const char* src_filename, int src_fd,
+    const char* dst_filename, int dst_fd)
+{
+    // Ensure destination file reached disk (kind of):
+    if (fsync(dst_fd) == -1)
+    {
+        fprintf(stderr, "Unable to sync file '%s': errno=%i (%s)",
+            dst_filename, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    // Close opened files:
+    if (close(src_fd) == -1)
+    {
+        fprintf(stderr, "Unable to close file '%s': errno=%i (%s)",
+            src_filename, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    if (close(dst_fd) == -1)
+    {
+        fprintf(stderr, "Unable to close file '%s': errno=%i (%s)",
+            dst_filename, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+#endif // MSUSEM_ASYNC_IO
---
+++ 02_async_io/sync-cp.c
@@ -0,0 +1,79 @@
+// No copyright. 2024, Vladislav Aleinik
+
+#include "common.h"
+
+#include <memory.h>
+
+//===========================
+// Copy procedure parameters
+//===========================
+
+#define READ_BLOCK_SIZE 4096U
+
+//===========================
+// Copy procedure parameters
+//===========================
+
+int main(int argc, char* argv[])
+{
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: sync-cp <src> <dst>");
+        exit(EXIT_FAILURE);
+    }
+
+    // Open source file and determine it's size:
+    int src_fd;
+    uint32_t src_size;
+    open_src_file(argv[1], &src_fd, &src_size);
+
+    // Create the destination file and allocate space on the disk:
+    int dst_fd;
+    open_dst_file(argv[2], &dst_fd, src_size);
+
+    //==============================
+    // Allocate intermediate buffer
+    //==============================
+
+    uint8_t* buffer = (uint8_t*) aligned_alloc(READ_BLOCK_SIZE, READ_BLOCK_SIZE);
+    if (buffer == NULL)
+    {
+        fprintf(stderr, "Unable to allocate aligned buffer\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //=====================
+    // Actual file copying
+    //=====================
+
+    for (uint32_t i = 0U; i < src_size;)
+    {
+        ssize_t bytes_read = read(src_fd, buffer, READ_BLOCK_SIZE);
+        if (bytes_read == -1)
+        {
+            fprintf(stderr, "Unable to read block [%x, %x)\n", i, i + READ_BLOCK_SIZE);
+            exit(EXIT_FAILURE);
+        }
+
+        ssize_t bytes_written = write(dst_fd, buffer, bytes_read);
+        if (bytes_written == -1 || bytes_written != bytes_read)
+        {
+            fprintf(stderr, "Unable to write block [%x, %lx)\n", i, i + bytes_read);
+            exit(EXIT_FAILURE);
+        }
+
+        i += bytes_read;
+        if (bytes_read != READ_BLOCK_SIZE)
+        {
+            break;
+        }
+    }
+
+    //============================
+    // End of actual file copying
+    //============================
+
+    close_src_dst_files(argv[1], src_fd, argv[2], dst_fd);
+
+    return EXIT_SUCCESS;
+}
